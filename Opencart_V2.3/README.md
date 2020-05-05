fbfd0ec3ced11d2923145665320e9059741f8dae
Rahul Verma <rahulverma0008@gmail.com>
2020-05-05 13:04:46 +0530

    added for opencart version 2.3

---
+++ Opencart_V2.3/README.md
@@ -0,0 +1,19 @@
+# Multiple Geo Zone Shipping
+
+## How To Install
+1) In your OpenCart admin panel go to Extensions > Extension Installer
+2) Upload the file downloaded ocmod.zip file
+3) Go to Extensions > Modifications, refresh your modification by clicking on blue Refresh button on top right corner
+4) Go to Extensions > Extensions, choose Order Totals from extension type
+5) You will see Pay Rest On Arrival module there, click the Install button
+6) After installing finished, click on edit button, configure this method as you like.
+Congratulations! Pay Rest On Arrival module is now installed. Customer will see this in frontend as option in Payment Method tab on checkout page, and after opting for Pay Rest On Arrival, customer will only need to pay Partial Payment as per your configuration.
+
+
+## Features
+1. Easy to install and setup
+2. Enable Partial Payment with any Payment Method
+3. Set different amount for different Payment Method
+4. Set with either Flat Amount or Percentage.
+5. Multiple Language Support
+6. Developed with OCMOD, which means this extension does not modify any core file of Opencart. So you can upgrade your Opencart version without losing this extension.
---
+++ Opencart_V2.3/upload/admin/language/en-gb/extension/total/prod.php
@@ -0,0 +1,43 @@
+<?php
+// Heading
+$_['heading_title']		= 'Pay Rest On Arrival';
+
+// Text
+$_['text_extension']   = 'Extensions';
+$_['text_total']		= 'Order Totals';
+$_['text_success']		= 'Success: You have modified Pay Rest On Arrival!';
+$_['text_edit']			= 'Edit Pay Rest On Arrival';
+
+$_['text_percentage']	= 'Percentage';
+$_['text_flat_amount']	= 'Flat Amount';
+
+// Entry
+$_['entry_total']		= 'Total';
+$_['entry_geo_zone']	= 'Geo Zone';
+$_['entry_text_opt']	= 'Display Text to Choose';
+$_['entry_text_total']	= 'Display Text for Amount';
+$_['entry_status']		= 'Status';
+$_['entry_sort_order']	= 'Sort Order';
+
+
+$_['entry_text_opt_default']	= 'I\'ll Pay Rest On Arrival';
+$_['entry_text_total_default']	= 'Pay Rest On Arrival (%s)';
+
+// Column
+$_['column_payment_method']	= 'Payment Method';
+$_['column_payment_type']	= 'Type';
+$_['column_payment_amount']	= 'Amount';
+
+
+// Help
+$_['help_entry_total']		= 'The checkout total the order must reach before Pay Rest On Arrival or Partial Payment  becomes active.';
+$_['help_entry_text_opt']	= 'This text will be displayed in Payment Method tab during checkout to let customer opt Pay Rest On Arrival';
+$_['help_entry_text_total']	= 'This text will be displayed for Total field during checkout.';
+$_['help_column_payment_method']= 'Payment method you want to enable for Pay Rest On Arrival or Partial Payment';
+$_['help_column_payment_type']	= 'This can be either Flat Amount or Percentage';
+$_['help_column_payment_amount']= 'Enter the amount that you want to collect from customer on Arrival';
+
+// Error
+$_['error_permission']	= 'Warning: You do not have permission to modify Pay Rest On Arrival!';
+$_['error_percentage']	= 'Percentage must be between 0 and 100!';
+$_['error_amount']		= 'Enter valid amount in number';
